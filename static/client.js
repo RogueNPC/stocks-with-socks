508e3d62a3ac648c1ee03e8c6948a041db3fd2ba
Jackie <taekwondo.chan@gmail.com>
2021-10-01 04:01:46 -0700

    more data & styling

--- static/client.js
+++ static/client.js
@@ -7,16 +7,25 @@ $(document).ready(() => {
 	});
 
 	socket.on("SEND_DATA", (data) => {
-		console.log(`Recieved data: ${data}`);
 		// Erase old containers
 		$("#stock-container").empty();
+
 		// Create new containers
 		for (let i = 0; i < data.length; i++) {
+			let buy;
+			if (data[i]["data"]["price"] == "Up") {
+				buy = "BUY BUY BUY";
+			} else {
+				buy = "SELL SELL SELL";
+			}
+
 			$("#stock-container").append(`
             <div class="stock-card">
-                <h2 style="text-align: center;">${data[i]["name"]}</h2>
+                <h2 style="text-align: center; font-weight: bold;">${data[i]["name"]}</h2>
+                <h3 style="text-align: center;">${buy}</h3>
                 <ul>
                 <li>Current price: ${data[i]["data"]["current"]}</li>
+                <li>Opening price: ${data[i]["data"]["open"]}</li>
                 <li>High price: ${data[i]["data"]["high"]}</li>
                 <li>Low price: ${data[i]["data"]["low"]}</li>
                 </ul>
