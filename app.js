467f7bd7c6d490550dd2c03668240e9540959119
Jackie <taekwondo.chan@gmail.com>
2021-09-30 20:48:06 -0700

    fix broken socket, update api promise chains

--- app.js
+++ app.js
@@ -1,84 +1,76 @@
 require("dotenv").config();
 const finnhub = require("finnhub");
 const express = require("express");
-const http = require("http");
 const app = express();
-
-// init socket.io
+const http = require("http");
 const server = http.createServer(app);
-const io = require("socket.io")(server);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 // Set up express static folder
 app.use(express.static("static"));
 
-// Routes
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "static/index.html");
+	res.sendFile(__dirname + "/static/index.html");
 });
 
 io.on("connection", (socket) => {
-	console.log(`User Connected`);
-
-  socket.on('API_CALL', () => {
-    console.log("GETTING DATA")
-    // getQuotes()
-    // .then((data) => {
-    //   console.log("SENDING DATA")
-    //   io.emit('SEND_DATA', data)
-    // })
-  })
-
-	socket.on("disconnect", () => {
-		console.log("User Disconnected");
+	socket.on("API_CALL", () => {
+		getQuotes().then((data) => {
+			io.emit("SEND_DATA", data);
+		});
 	});
 });
 
-let getQuotes = () => {
-  const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-  api_key.apiKey = ""
-  const finnhubClient = new finnhub.DefaultApi()
+const getQuotes = () => {
+	const api_key = finnhub.ApiClient.instance.authentications["api_key"];
+	api_key.apiKey = process.env.API_KEY;
+	const finnhubClient = new finnhub.DefaultApi();
 
-  let promises = []
+	return new Promise((resolve, reject) => {
+		let promises = [];
 
-  promises.push(
-    new Promise((resolve, reject) => {
-      finnhubClient.quote('AAPL', (error, data, response) => {
-        resolve({
-          "name": "Apple",
-          "data": {
-            "current": data['c'],
-            "high": data['h'],
-            "low": data['l']
-          }
-        })
-      })
-    })
-  )
+		promises.push(
+			new Promise((resolve, reject) => {
+				finnhubClient.quote("AAPL", (error, data, response) => {
+					resolve({
+						name: "Apple",
+						data: {
+							current: data["c"],
+							high: data["h"],
+							low: data["l"],
+						},
+					});
+				});
+			})
+		);
 
-  promises.push(
-    new Promise((resolve, reject) => {
-      finnhubClient.quote('GME', (error, data, response) => {
-        resolve({
-          "name": "Gamestop",
-          "data": {
-            "current": data['c'],
-            "high": data['h'],
-            "low": data['l']
-          }
-        })
-      })
-    })
-  )
+		promises.push(
+			new Promise((resolve, reject) => {
+				finnhubClient.quote("GME", (error, data, response) => {
+					resolve({
+						name: "Gamestop",
+						data: {
+							current: data["c"],
+							high: data["h"],
+							low: data["l"],
+						},
+					});
+				});
+			})
+		);
 
-  Promise.all(promises).then(data => {
-    return data
-  })
-  .catch(err => {
-    console.log(err)
-  })
-}
+		Promise.all(promises)
+			.then((data) => {
+				resolve(data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
+};
 
 const PORT = 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
--- static/client.js
+++ static/client.js
@@ -1,27 +1,26 @@
 $(document).ready(() => {
-    const socket = io.connect();
-    console.log(socket)
- 
-    document.getElementById('refresh-stocks-btn').addEventListener('click', function() {
-        console.log("Button was clicked")
-        socket.emit('API_CALL')
-    });
+	const socket = io.connect();
+	console.log(socket);
 
-    
-    socket.on('SEND_DATA', (data) => {
-        console.log(`Recieved data: ${data}`)
-        // Erase old containers
-        $('#stock-container').empty()
-        // Create new containers
-        for (let i = 0; i < data.length; i++) {
-            $('#stock-container').append(`
+	document.getElementById("refresh-stocks-btn").addEventListener("click", function () {
+		console.log("Button was clicked");
+		socket.emit("API_CALL");
+	});
+
+	socket.on("SEND_DATA", (data) => {
+		console.log(`Recieved data: ${data}`);
+		// Erase old containers
+		$("#stock-container").empty();
+		// Create new containers
+		for (let i = 0; i < data.length; i++) {
+			$("#stock-container").append(`
             <div class="stock-card">
-                <p>${data[i]['name']}</p>
-                <p>Current price: ${data[i]['data']['current']}</p>
-                <p>High price: ${data[i]['data']['high']}</p>
-                <p>Low price: ${data[i]['data']['low']}</p>
+                <p>${data[i]["name"]}</p>
+                <p>Current price: ${data[i]["data"]["current"]}</p>
+                <p>High price: ${data[i]["data"]["high"]}</p>
+                <p>Low price: ${data[i]["data"]["low"]}</p>
             </div>
-            `)
-        }
-    });
-})
+            `);
+		}
+	});
+});
